85a5328b82b94a70bd7b83c48a49fb63938c5af7
Fred <huertaserick@yahoo.ie>
2025-11-26 19:37:52 +0000

    Removed some of the details and changed it with just the status and value

--- wp-content/themes/astra-child/single-pet.php
+++ wp-content/themes/astra-child/single-pet.php
@@ -5,17 +5,26 @@ Template Name: Single Pets Archive Page
 
 get_header(); ?>
 
-<div class="single-pet">
+<div class="single-pet-post">
+    <div class="single-pet-image"><?php the_post_thumbnail('full'); ?></div>
     <h1><?php the_title(); ?></h1>
-    <?php the_post_thumbnail('large'); ?>
     <div class="pet-details">
-        <p>Breed: <?php echo esc_html(get_field('breed')); ?></p>
-        <p>Age: <?php echo esc_html(get_field('age')); ?></p>
-        <p>Status: <?php echo esc_html(get_field('status')); ?></p>
+        <p>
+            <i class="fa fa-question" aria-hidden="true"></i>
+            <?php
+            $value = get_field('status');
+            echo $value ? ucfirst($value) : ''; // first letter uppercase
+            ?>
+        </p>
+        <p>
+            <i class="fa fa-money" aria-hidden="true"></i>
+            €<?php echo esc_html(get_field('adoption_fee')); ?>
+        </p>
     </div>
     <div class="pet-content">
         <?php the_content(); ?>
     </div>
+    <button>Adopt Me 🐶</button>
 </div>
 
 <?php get_footer(); ?>
