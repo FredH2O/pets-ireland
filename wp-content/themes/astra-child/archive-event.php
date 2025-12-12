d321787c69a023e1711c94a30ff5a126684f937a
Fred <huertaserick@yahoo.ie>
2025-12-12 18:40:15 +0000

    styling for archive event cards

--- wp-content/themes/astra-child/archive-event.php
+++ wp-content/themes/astra-child/archive-event.php
@@ -5,27 +5,36 @@ Template Name: Archive Events
 get_header();
 ?>
 
-<?php
-if (have_posts()) :
-    while (have_posts()):
-        the_post(); ?>
+<div class="events-container">
 
-        <?php if (has_post_thumbnail()) : ?>
-            <?php the_post_thumbnail('full') ?>
-        <?php endif; ?>
+    <?php
+    if (have_posts()) :
+        while (have_posts()):
+            the_post(); ?>
 
-        <h2><?php the_title() ?></h2>
-        <p>Date: <?php the_field('event_date'); ?></p>
-        <p>Location: <?php the_field('event_location'); ?></p>
-        <div>
-            <?php the_content(); ?>
-        </div>
+            <div class="events-cards">
+                <?php if (has_post_thumbnail()) : ?>
+                    <?php the_post_thumbnail('full') ?>
+                <?php endif; ?>
 
-<?php
-    endwhile;
-else :
-    echo '<p>No posts found!</p>';
-endif;
-?>
+                <h2><?php the_title() ?></h2>
+                <p>Date: <?php the_field('event_date'); ?></p>
+                <p>Location: <?php the_field('location'); ?></p>
+                <div>
+                    <?php the_excerpt(); ?>
+                </div>
+            </div>
+
+
+    <?php
+        endwhile;
+    else :
+        echo '<p>No posts found!</p>';
+    endif;
+    ?>
+
+</div>
+
+
 
 <?php get_footer(); ?>
