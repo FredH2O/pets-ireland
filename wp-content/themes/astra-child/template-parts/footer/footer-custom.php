7704dbaa4e994dad0d52ea99cd09823a2b8d3eec
Fred <huertaserick@yahoo.ie>
2026-01-12 19:21:26 +0000

    feat: footer structure

--- wp-content/themes/astra-child/template-parts/footer/footer-custom.php
+++ wp-content/themes/astra-child/template-parts/footer/footer-custom.php
@@ -1,5 +1,66 @@
 <footer>
     <div>
-        <p>&copy; <?php echo date('Y') ?> Built the right way.</p>
+
+        <!-- Branding / Info / Newsletter -->
+        <div class="first-footer-div">
+
+            <!-- Logo-->
+            <div>
+                <?php if (function_exists('the_custom_logo')) {
+                    the_custom_logo();
+                } ?>
+            </div>
+
+            <!-- About Info -->
+            <div>
+                <p>The ISPCA works to prevent cruelty to animals all over the Republic of Ireland</p>
+
+                <p>
+                    <span>Report Cruelty</span>
+                    Call: 0818 515 515 (or +353 43 33 25035 from outside of Republic of Ireland)
+                </p>
+            </div>
+
+            <!-- Newsletter -->
+            <div class="newsletter-wrapper">
+                <h3>Join Our Family and Get Updates on Rescued Pets</h3>
+                <?php echo do_shortcode('[newsletter_form]'); ?>
+            </div>
+        </div>
+
+        <!-- Primary Navigation Links -->
+        <div class="second-footer-div">
+            <ul>
+                <li>Homepage</li>
+                <li>Who We Are</li>
+                <li>How You Can Help</li>
+                <li>Adopt a Pet</li>
+                <li>Lost & Found</li>
+                <li>Shop</li>
+                <li>Press & Media Enquiries</li>
+            </ul>
+        </div>
+
+        <!-- Secondary Links / Legal -->
+        <div>
+            <ul>
+                <li>About us</li>
+                <li>Contact us</li>
+                <li>Careers</li>
+                <li>FAQ</li>
+                <li>Animal Welfare Policies</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </div>
+
+        <!-- Social Links -->
+        <div>
+            <!-- Social links / Icons here -->
+            <p>Follow us on Instagram</p>
+            <!--- Will have 4 images here 2/2 --->
+        </div>
+
     </div>
+    <!-- Copyrights -->
+    <p>&copy; <?php echo date('Y') ?> Built the right way.</p>
 </footer>
